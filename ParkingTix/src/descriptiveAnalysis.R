2c8bf47479aa2273e1528932bbf3e1406783640f
Matthew Schumwinger <milwaukeestat@gmail.com>
2013-07-13 01:47:39 -0500

    Added MapBox Tiles

--- ParkingTix/src/descriptiveAnalysis.R
+++ ParkingTix/src/descriptiveAnalysis.R
@@ -30,8 +30,22 @@ view(dupes)
 null <- is.na(data)
 table(null) #no missing values
 
-# select random subset to geocode
+# select random subset to geocode and export for Mapquest geocode
 set.seed(1133)
 sampleKey <- sample(1:743833, 10000)
 subset <- data[c(sampleKey),]
 write.csv(subset,"./data/subset10K.csv")
+
+# import geocoded file and analyze
+#subset10Kgeo <- read.csv("./data/subset10Kgeo.csv")
+table(is.na(subset10Kgeo$geo_longitude)) # 678 not geocoded 6.78%
+noGeo <- subset(subset10Kgeo,is.na(geo_longitude))
+sort(table(noGeo$LOCATIONDESC1)) # not to many repeats, although "1710 N. Arlinton Lot" is most frequent
+# I may want to process these a little and rerun geocode later
+colnames(subset10Kgeo)[1] <- "rownum"  #change field names for MapBox import
+colnames(subset10Kgeo)[8] <- "longitude"
+colnames(subset10Kgeo)[9] <- "latitude"
+colnames(subset10Kgeo)[10] <- "accuracy"
+write.csv(subset10Kgeo,"./data/subset10Kgeo.csv", row.names = FALSE) # this avoids blank col name for row.names column,
+#which stymies the MapBox csv importer
+
